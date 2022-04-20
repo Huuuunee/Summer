20920570af6b6b932cae2911830da0703cd2737d
시훈 <81404026+sihun96@users.noreply.github.com>
2022-04-20 22:56:26 +0900

    :sparkles: 유저들 찾기 + 유저아이디로 유저찾기 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -24,6 +25,17 @@ public class MemberService {
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 있는 유저입니다");
                 });
+    }
+
+    /**
+     * 전체 유저 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 
 }
