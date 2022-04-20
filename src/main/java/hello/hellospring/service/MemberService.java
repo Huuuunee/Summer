14ee6f37177c0f0e8bcc1ec50d7c13e1b4fc9c79
시훈 <81404026+sihun96@users.noreply.github.com>
2022-04-20 22:51:58 +0900

    :poop: 리팩토링 로직

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -14,12 +14,16 @@ public class MemberService {
     * 회원가입
      */
     public Long join(Member member){
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 있는 유저입니다");
-        });
+        validateDuplicateMember(member);//중복 회원 검증
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 있는 유저입니다");
+                });
     }
 
 }
