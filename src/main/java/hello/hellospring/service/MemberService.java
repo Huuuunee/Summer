f7e2c648e48cdd1d6aa1ff27bb3a7aef91411f0a
시훈 <81404026+sihun96@users.noreply.github.com>
2022-04-20 22:48:11 +0900

    :sparkles: 회원가입 기능 + 이미 있는 유저는 에러문

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,25 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+    * 회원가입
+     */
+    public Long join(Member member){
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 있는 유저입니다");
+        });
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+}
