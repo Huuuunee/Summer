91355ce8d33270296ec8122e749c7dc4a4551674
Huuuunee <s21024@gsm.hs.kr>
2022-05-12 14:07:32 +0900

    :sparkles: 목록보기 기능 추가

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     // 아무것도 안되어있을떼 index.html이 나와야 하지만 안나오는 이유는 스프링 컨테이너 검색할때 이미 아무것도 없는 get / 요청이 있어서 home.html이 반환된다
-    @GetMapping("/")
+    @GetMapping
     public String home(){
         return "home";
     }
